3c232d3043d483a769a1f6c5733ef6f475f45103
Kyle Swanson <kylophone@gmail.com>
2015-06-16 19:45:16 -0500

    fix segfault with no args

--- spectrophoto.c
+++ spectrophoto.c
@@ -45,7 +45,7 @@ float get_pixel_intensity(unsigned char *data, int n)
 
 int main(int argc, char **argv)
 {
-    if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--h") || !strcmp(argv[1], "-help") || !strcmp(argv[1], "--help")) {
+    if (argc == 2 && (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--h") || !strcmp(argv[1], "-help") || !strcmp(argv[1], "--help"))) {
     fprintf(stderr, "Usage: spectrophoto <inputfile> <outputfile>\n");
     fprintf(stderr, "https://github.com/kylophone/spectrophoto\n");
     return(0);
